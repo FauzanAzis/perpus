ca71c5ae53051ce94ec235bb9af7842ca80a984f
Fauzan Azis <fauzan.binas@gmail.com>
2018-11-01 14:13:50 +0700

    init MenuServiceProvider

---
+++ app/Providers/MenuServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\ServiceProvider;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+
+class MenuServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Dispatcher $events)
+    {
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MAIN NAVIGATION');
+            $event->menu->add([
+                'text' => 'Buku',
+                'route' => 'buku.index',
+                'icon' => 'book'
+            ]);
+            $event->menu->add([
+                'text' => 'Klasifikasi',
+                'route' => 'klasifikasi.index',
+                'icon' => 'sitemap'
+            ]);
+            $event->menu->add([
+                'text' => 'Penerbit',
+                'route' => 'penerbit.index',
+                'icon' => 'flag-checkered '
+            ]);
+            $event->menu->add([
+                'text' => 'Pengarang',
+                'route' => 'pengarang.index',
+                'icon' => 'users'
+            ]);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
